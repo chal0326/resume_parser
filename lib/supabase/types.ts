bff6ce382e139f4ddecb80c40f5979ad47b8c975
Cody L Hall <chal032687@gmail.com>
2024-10-24 05:57:39 -0400

    commit to main

--- lib/supabase/types.ts
+++ lib/supabase/types.ts
@@ -6,134 +6,205 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
-export interface Database {
+export type Database = {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
   public: {
     Tables: {
-      parsed_resumes: {
+      awards: {
         Row: {
-          id: string
-          user_id: string
+          award: string
           created_at: string
-        }
-        Insert: {
-          id?: string
-          user_id: string
-          created_at?: string
-        }
-        Update: {
-          id?: string
-          user_id?: string
-          created_at?: string
-        }
-      }
-      work_experiences: {
-        Row: {
           id: string
           resume_id: string
-          job_title: string
-          company: string
-          start_date: string
-          end_date: string | null
-          created_at: string
         }
         Insert: {
+          award: string
+          created_at?: string
           id?: string
           resume_id: string
-          job_title: string
-          company: string
-          start_date: string
-          end_date?: string | null
-          created_at?: string
         }
         Update: {
+          award?: string
+          created_at?: string
           id?: string
           resume_id?: string
-          job_title?: string
-          company?: string
-          start_date?: string
-          end_date?: string | null
-          created_at?: string
         }
-      }
-      job_descriptions: {
-        Row: {
-          id: string
-          work_experience_id: string
-          description: string
-          created_at: string
-        }
-        Insert: {
-          id?: string
-          work_experience_id: string
-          description: string
-          created_at?: string
-        }
-        Update: {
-          id?: string
-          work_experience_id?: string
-          description?: string
-          created_at?: string
-        }
-      }
-      skills: {
-        Row: {
-          id: string
-          resume_id: string
-          skill: string
-          created_at: string
-        }
-        Insert: {
-          id?: string
-          resume_id: string
-          skill: string
-          created_at?: string
-        }
-        Update: {
-          id?: string
-          resume_id?: string
-          skill?: string
-          created_at?: string
-        }
+        Relationships: [
+          {
+            foreignKeyName: "awards_resume_id_fkey"
+            columns: ["resume_id"]
+            isOneToOne: false
+            referencedRelation: "parsed_resumes"
+            referencedColumns: ["id"]
+          },
+        ]
       }
       certifications: {
         Row: {
-          id: string
-          resume_id: string
           certification: string
           created_at: string
+          id: string
+          resume_id: string
         }
         Insert: {
-          id?: string
-          resume_id: string
           certification: string
           created_at?: string
+          id?: string
+          resume_id: string
         }
         Update: {
-          id?: string
-          resume_id?: string
           certification?: string
           created_at?: string
-        }
-      }
-      awards: {
-        Row: {
-          id: string
-          resume_id: string
-          award: string
-          created_at: string
-        }
-        Insert: {
-          id?: string
-          resume_id: string
-          award: string
-          created_at?: string
-        }
-        Update: {
           id?: string
           resume_id?: string
-          award?: string
-          created_at?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "certifications_resume_id_fkey"
+            columns: ["resume_id"]
+            isOneToOne: false
+            referencedRelation: "parsed_resumes"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      job_descriptions: {
+        Row: {
+          created_at: string
+          description: string
+          id: string
+          work_experience_id: string
+        }
+        Insert: {
+          created_at?: string
+          description: string
+          id?: string
+          work_experience_id: string
+        }
+        Update: {
+          created_at?: string
+          description?: string
+          id?: string
+          work_experience_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "job_descriptions_work_experience_id_fkey"
+            columns: ["work_experience_id"]
+            isOneToOne: false
+            referencedRelation: "work_experiences"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      parsed_resumes: {
+        Row: {
+          created_at: string
+          id: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+      skills: {
+        Row: {
+          created_at: string
+          id: string
+          resume_id: string
+          skill: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          resume_id: string
+          skill: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          resume_id?: string
+          skill?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "skills_resume_id_fkey"
+            columns: ["resume_id"]
+            isOneToOne: false
+            referencedRelation: "parsed_resumes"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      work_experiences: {
+        Row: {
+          company: string
+          created_at: string
+          end_date: string | null
+          id: string
+          job_title: string
+          resume_id: string
+          start_date: string
+        }
+        Insert: {
+          company: string
+          created_at?: string
+          end_date?: string | null
+          id?: string
+          job_title: string
+          resume_id: string
+          start_date: string
+        }
+        Update: {
+          company?: string
+          created_at?: string
+          end_date?: string | null
+          id?: string
+          job_title?: string
+          resume_id?: string
+          start_date?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "work_experiences_resume_id_fkey"
+            columns: ["resume_id"]
+            isOneToOne: false
+            referencedRelation: "parsed_resumes"
+            referencedColumns: ["id"]
+          },
+        ]
       }
     }
     Views: {
@@ -145,5 +216,105 @@ export interface Database {
     Enums: {
       [_ in never]: never
     }
+    CompositeTypes: {
+      [_ in never]: never
+    }
   }
 }
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
